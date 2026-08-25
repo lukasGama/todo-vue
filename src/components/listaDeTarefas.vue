b0fe10d185c4a416dc867226440abee5f13d8770
Lucas Gama <97904862+lukasGama@users.noreply.github.com>
2026-08-25 19:50:24 -0300

    alterando falhas

--- src/components/listaDeTarefas.vue
+++ src/components/listaDeTarefas.vue
@@ -4,7 +4,10 @@
 
 <template>
     <ul class="list-group mt-4">
-        <li class="list-group-item" v-for="tarefa in props.tarefas"><!--usando a diretiva-->
+        <li v-if="props.tarefas.length === 0" class="list-group-item text-center" >
+            Não tem tarefas Pendentes
+        </li>
+        <li v-for="tarefa in props.tarefas" :key="tarefa.titulo" class="list-group-item"><!--usando a diretiva-->
             <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
                 :id="tarefa.titulo" type="checkbox"><!--Ultilizando o biden, começa com os dois pontos-->
             <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo"><!--ms= margin start-->
